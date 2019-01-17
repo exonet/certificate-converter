5681ffb420cca84cd39afeb6a761e1e229ef3c12
Ramon Splithof <r.splithof@exonet.nl>
2019-01-17 11:20:00 +0100

    Add support to export to PKCS12 format

--- src/Formats/Pkcs12.php
+++ src/Formats/Pkcs12.php
@@ -1,20 +1,26 @@
 <?php
 
-namespace Exonet\Format;
+namespace Exonet\SslConverter\Formats;
+
+use Exonet\SslConverter\Exceptions\MissingRequiredInformation;
 
 class Pkcs12 implements FormatInterface
 {
-    public function __construct()
+    public function export(Plain $certificate, array $options) : string
     {
-    }
+        $key = $certificate->getKey();
+        $crt = $certificate->getCrt();
+        $caBundle = $certificate->getCaBundle();
+        $password = isset($options['password']) ? $options['password'] : false;
 
-    public function export(Php $certificate, array $options)
-    {
-        // TODO: Implement export() method.
-    }
+        if (!$key || !$crt || !$caBundle || !$password) {
+            throw new MissingRequiredInformation();
+        }
 
-    public function getPlain() : Php
-    {
-        // TODO: Implement getPlain() method.
+        if (!openssl_pkcs12_export($crt, $pkc12, $key, $password, ['extracerts' => $caBundle])) {
+            throw new InvalidArgumentException('Invalid certificate provided.');
+        };
+
+        return $pkc12;
     }
 }
