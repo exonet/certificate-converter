a1ae8aae507158ea16c5dc0cd616f1112c9c9721
Tristan <t.siebers@exonet.nl>
2019-02-22 11:21:34 +0100

    Apply PR comments

--- src/Formats/Pem.php
+++ src/Formats/Pem.php
@@ -30,10 +30,10 @@ class Pem extends AbstractFormat
             throw new MissingRequiredInformation('The following fields are required for PEM: CRT, CA Bundle.');
         }
 
-        $possibleWhitelines = ["\x0D", "\r", "\n", '\n', '\r'];
+        $possibleNewLines = ["\x0D", "\r", "\n", '\n', '\r'];
 
         // Strip all kind of (wrong) newlines, indentations, etc. and create a correct certificate from the CRT.
-        $x509cert = str_replace($possibleWhitelines, '', $crt);
+        $x509cert = str_replace($possibleNewLines, '', $crt);
         $x509cert = str_replace('-----BEGIN CERTIFICATE-----', '', $x509cert);
         $x509cert = str_replace('-----END CERTIFICATE-----', '', $x509cert);
         $x509cert = str_replace(' ', '', $x509cert);
@@ -41,9 +41,9 @@ class Pem extends AbstractFormat
 
         // Clean the newlines in the key.
         if ($key) {
-            $x509key = str_replace($possibleWhitelines, '', $key);
-            $x509key = str_replace('-----BEGIN PRIVATE KEY-----', "", $x509key);
-            $x509key = str_replace('-----END PRIVATE KEY-----', "", $x509key);
+            $x509key = str_replace($possibleNewLines, '', $key);
+            $x509key = str_replace('-----BEGIN PRIVATE KEY-----', '', $x509key);
+            $x509key = str_replace('-----END PRIVATE KEY-----', '', $x509key);
             $x509key = str_replace(' ', '', $x509key);
             $x509key = "-----BEGIN PRIVATE KEY-----\n".chunk_split($x509key, 64, "\n")."-----END PRIVATE KEY-----\n";
         }
